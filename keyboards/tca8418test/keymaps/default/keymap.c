350673054b4f57b1fcb65dcbc1bd96fc578ddc74
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2019-12-06 12:11:41 -0800

    update keymap, speed up mouse keys

--- keyboards/tca8418test/keymaps/default/keymap.c
+++ keyboards/tca8418test/keymaps/default/keymap.c
@@ -4,20 +4,180 @@
 #include QMK_KEYBOARD_H
 #include "tca8418test.h"
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
+enum layer_id {
+  LAYER_COLEMAK,
+  LAYER_QWERTY,
+  LAYER_SYMBOLS,
+  LAYER_FNARROWS,
+  LAYER_MOUSEMACRO,
+};
 
-               /* RESET, KC_W, KC_E, KC_R, KC_T, KC_Y,        RESET, KC_W, KC_E, KC_R, KC_T, KC_Y, */
-               /* KC_U, KC_I, KC_O, KC_P, KC_A, KC_S,         KC_U, KC_I, KC_O, KC_P, KC_A, KC_S, */
-               /* KC_LSFT, KC_RSFT, KC_H, KC_D, KC_X, KC_K,   KC_LSFT, KC_RSFT, KC_H, KC_D, KC_X, KC_K, */
-               /* KC_L, KC_Z, KC_X, KC_C, KC_V, RESET,        KC_L, KC_Z, KC_X, KC_C, KC_V, RESET */
+enum macro_id {
+  ECHOH,
+  GITCOMMIT,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Qwerty */
-               KC_LSFT,    KC_RSFT,     KC_H,    KC_X,    KC_D,    RESET,   RESET,   KC_J,    KC_L,    KC_U,     KC_Y,     KC_SCLN, \
-               KC_A,    KC_R,     KC_S,    KC_T,    KC_D,    RESET,   RESET,   KC_H,    KC_N,    KC_E,     KC_I,     KC_O, \
-               KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    RESET,   RESET,   KC_K,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, \
-               KC_FN7,  KC_LGUI,  KC_TAB,  KC_FN1,  KC_FN6,  KC_FN3,  KC_FN4,  KC_FN5,  KC_FN0,  KC_MINS,  KC_QUOT,  KC_FN8),
+
+[LAYER_COLEMAK] = LAYOUT(
+                         KC_Q,   KC_W,    KC_F,   KC_P,   KC_G,   RESET,  /* */ RESET,  KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, \
+                         KC_A,   KC_R,    KC_S,   KC_T,   KC_D,   RESET,  /* */ RESET,  KC_H,   KC_N,   KC_E,    KC_I,    KC_O, \
+                         KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,   KC_FN3, /* */ KC_FN4, KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, \
+                         KC_FN7, KC_LGUI, KC_TAB, KC_FN1, KC_FN6, RESET,  /* */ RESET,  KC_FN5, KC_FN0, KC_MINS, KC_QUOT, KC_FN8),
+
+// Q  W  E  R  T      Y  U  I     O    P
+// A  S  D  F  G      H  J  K     L    SCLN
+// Z  X  C  V  B      N  M  COMM  DOT  SLSH
+
+[LAYER_QWERTY] = LAYOUT(
+                        KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,   KC_TRNS, /* */ KC_TRNS, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P, \
+                        KC_A,   KC_S,    KC_D,   KC_F,   KC_G,   KC_TRNS, /* */ KC_TRNS, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, \
+                        KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,   KC_FN3,  /* */ KC_FN4,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, \
+                        KC_FN7, KC_LGUI, KC_TAB, KC_FN1, KC_FN6, KC_TRNS, /* */ KC_TRNS, KC_FN5, KC_FN0, KC_MINS, KC_QUOT, KC_FN8),
+
+// * 7 8 9 0  | [ ] # !
+// + 4 5 6 ~  @ ( ) ^ $
+// = 1 2 3 `  & { } % BSLASH
+//   0 .          - '
+
+[LAYER_SYMBOLS] = LAYOUT(
+                         S(KC_8),   KC_7, KC_8,   KC_9,    KC_0,        KC_NO, /* */ KC_NO, S(KC_BSLS), KC_LBRC,    KC_RBRC,    S(KC_3),    S(KC_1), \
+                         S(KC_EQL), KC_4, KC_5,   KC_6,    S(KC_GRAVE), KC_NO, /* */ KC_NO, S(KC_2),    S(KC_9),    S(KC_0),    S(KC_6),    S(KC_4), \
+                         KC_EQL,    KC_1, KC_2,   KC_3,    KC_GRAVE,    KC_NO, /* */ KC_NO, S(KC_7),    S(KC_LBRC), S(KC_RBRC), S(KC_5),    KC_BSLS, \
+                         KC_TRNS,   KC_0, KC_DOT, KC_TRNS, KC_TRNS,     KC_NO, /* */ KC_NO, KC_TRNS,    KC_TRNS,    S(KC_MINS), S(KC_QUOT), KC_TRNS),
+
+[LAYER_FNARROWS] = LAYOUT(
+                          KC_INS,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO, /* */ KC_NO, KC_HOME, KC_RGHT,    KC_END,     KC_PGUP,    KC_PSCR, \
+                          KC_DEL,  KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_NO, /* */ KC_NO, KC_LEFT, KC_DOWN,    KC_UP,      KC_PGDN,    KC_PAUSE, \
+                          KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F12,  KC_NO, /* */ KC_NO, KC_VOLD, KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN4, \
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, /* */ KC_NO, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    RESET),
+
+[LAYER_MOUSEMACRO] = LAYOUT(
+                            KC_FN12, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_TRNS, /* */ KC_TRNS, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, \
+                            KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_TRNS, /* */ KC_TRNS, KC_NO, KC_NO, KC_FN10, KC_NO, KC_NO, \
+                            KC_NO,   KC_NO,   KC_FN11, KC_BTN3, KC_NO,   KC_TRNS, /* */ KC_TRNS, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, \
+                            KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_BTN1, KC_NO,   /* */ KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO, KC_FN8),
 };
+
+// Macro definition
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+
+  /*
+  // debug
+  xprintf("------------\n");
+  xprintf("key row: %u\n", record->event.key.row);
+  xprintf("key col: %u\n", record->event.key.col);
+  xprintf("pressed: %u\n", record->event.pressed);
+  */
+
+  switch (id) {
+  case ECHOH:
+    return (record->event.pressed ?
+            MACRO( D(LCTRL), T(C), U(LCTRL), T(E), T(C), T(H), T(O), T(SPC), T(MINS), T(E), T(SPC),
+                   D(LSHIFT), T(QUOT), U(LSHIFT), T(BSLS), T(0), T(3), T(3), D(LSHIFT), T(9), U(LSHIFT), T(0), D(LSHIFT), T(QUOT), U(LSHIFT), T(SPC),
+                   T(SCLN), T(SPC), T(C), T(L), T(E), T(A), T(R), T(ENT),
+                   END) :
+            MACRO_NONE);
+
+  case GITCOMMIT:
+    return (record->event.pressed ?
+            MACRO( D(LCTRL), T(C), U(LCTRL),
+                   T(G), T(I), T(T), T(SPC), T(C), T(O), T(M), T(M), T(I), T(T), T(SPC), T(MINS), T(A), T(ENT),
+                   END) :
+            MACRO_NONE);
+
+    /*
+  // ruby -e 'Dir["*"].map{|d|`mv #{d} #{d.split("").shuffle.join}`}'
+  case SHUFFLEFILES:
+    return (record->event.pressed ?
+            MACRO( D(LCTRL), T(C), U(LCTRL), T(R), T(U), T(B), T(Y), T(SPC), T(MINUS), T(E), T(SPC),
+                   T(QUOT),
+                   D(LSHIFT), T(D), U(LSHIFT),
+                   T(I),
+                   T(R),
+                   T(LBRACKET),
+                   D(LSHIFT), T(QUOT), U(LSHIFT),
+                   D(LSHIFT), T(8), U(LSHIFT),
+                   D(LSHIFT), T(QUOT), U(LSHIFT),
+                   T(RBRACKET),
+                   T(DOT),
+                   T(M),
+                   T(A),
+                   T(P),
+                   D(LSHIFT), T(LBRACKET), U(LSHIFT),
+                   D(LSHIFT), T(BSLASH), U(LSHIFT),
+                   T(D),
+                   D(LSHIFT), T(BSLASH), U(LSHIFT),
+                   T(GRAVE),
+                   T(M),
+                   T(V),
+                   T(SPC),
+                   D(LSHIFT), T(3), U(LSHIFT),
+                   D(LSHIFT), T(LBRACKET), U(LSHIFT),
+                   T(D),
+                   D(LSHIFT), T(RBRACKET), U(LSHIFT),
+                   T(SPC),
+                   D(LSHIFT), T(3), U(LSHIFT),
+                   D(LSHIFT), T(LBRACKET), U(LSHIFT),
+                   T(D),
+                   T(DOT),
+                   T(S),
+                   T(P),
+                   T(L),
+                   T(I),
+                   T(T),
+                   D(LSHIFT), T(9), U(LSHIFT),
+                   D(LSHIFT), T(QUOT), U(LSHIFT),
+                   D(LSHIFT), T(QUOT), U(LSHIFT),
+                   D(LSHIFT), T(0), U(LSHIFT),
+                   T(DOT),
+                   T(S),
+                   T(H),
+                   T(U),
+                   T(F),
+                   T(F),
+                   T(L),
+                   T(E),
+                   T(DOT),
+                   T(J),
+                   T(O),
+                   T(I),
+                   T(N),
+                   D(LSHIFT), T(RBRACKET), U(LSHIFT),
+                   T(GRAVE),
+                   D(LSHIFT), T(RBRACKET), U(LSHIFT),
+                   T(QUOT),
+                   T(ENT),
+                   END) :
+            MACRO_NONE);
+    */
+
+  }
+  return MACRO_NONE;
+}
+
+
+const uint16_t PROGMEM fn_actions[] = {
+  /* [0] = ACTION_LAYER_TAP_TOGGLE(1), */
+  /* [1] = ACTION_LAYER_TAP_TOGGLE(2), */
+  [0] = ACTION_LAYER_MOMENTARY(2),
+  [1] = ACTION_LAYER_MOMENTARY(3),
+  /* [2] = ACTION_FUNCTION(BOOTLOADER), */
+  [3] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
+  [4] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_ENT),
+  [5] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SPC),
+  [6] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),
+  [7] = ACTION_MODS_TAP_KEY(MOD_LGUI|MOD_LCTL|MOD_LALT, KC_ESC),
+  [8] = ACTION_LAYER_TAP_KEY(4, KC_ENT),
+  [9] = ACTION_MACRO(GITCOMMIT),
+  [10] = ACTION_MACRO(ECHOH),
+  [11] = ACTION_DEFAULT_LAYER_SET(0),
+  [12] = ACTION_DEFAULT_LAYER_SET(1),
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  /* if (id == BOOTLOADER) { */
+  /*   bootloader(); */
+  /* } */
+}
